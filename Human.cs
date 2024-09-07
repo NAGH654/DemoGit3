247d77c80fe35e2de8219eed31c1017b6a75c489
NAGH654 <huynguyen.dmx2004@gmail.com>
2024-09-07 18:26:06 +0700

    new C#

---
+++ Human.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstProgram
+{
+    class Human
+    {
+        private String name;
+        private int age;
+
+        public Human()
+        {
+            this.name = String.Empty;
+            this.age = 0;
+        }
+
+        public Human(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        public int Age { get => age; set => age = value; }
+        public string Name { get => name; set => name = value; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace MyFirstProgram {
+    class Program {
+        static void Main(string[] args)
+        {
+            /*Random random = new Random();
+            int a = 0;
+            int b = 0;
+            int score = 1;
+            Console.Write("enter number: ");
+            do {
+                b = random.Next(6)+1;
+                a = Convert.ToInt32(Console.ReadLine());
+                if (a != b)
+                {
+                    Console.Write("guess again: ");
+                    score++;
+                }
+            } while (a != b);
+            Console.WriteLine("you are correct");
+            Console.WriteLine("you guessed: " + score + " times");*/
+            Human human1 = new Human();
+            human1.Age = 10;
+            Console.WriteLine(human1.Age);
+            Console.ReadKey();
+        }
+    }
+}
